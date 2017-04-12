0434433b439ed09e6a274c827aaa9d18bf63b506
Steve Hamblett <steve.hamblett@linux.com>
2017-04-12 06:44:59 +0000

    Initial structure

---
+++ lib/coap.dart
@@ -0,0 +1,15 @@
+/*
+ * Package : Coap
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 12/04/2017
+ * Copyright :  S.Hamblett
+ */
+
+library coap;
+
+import 'dart:io';
+import 'package:typed_data/typed_data.dart';
+
+/// The Coap package exported interface
+
+part 'src/coap.dart';
