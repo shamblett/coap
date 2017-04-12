2a9ec6a6459c2e3172c1e4c6b33c7bbe8ab2fbd0
Steve Hamblett <steve.hamblett@linux.com>
2017-04-12 07:20:57 +0000

    Networking classes + main class

--- lib/src/coap.dart
+++ lib/src/coap.dart
@@ -9,4 +9,28 @@ part of coap;
 
 /// The Coap package API
 class Coap {
+
+  /// The network interface to use
+  Network _network;
+
+  NetworkUDP get udpNetwork => _network as NetworkUDP;
+
+  Network get network => _network;
+
+  set udpNetwork(NetworkUDP net) => _network = net;
+
+  set network(Network net) => _network = net;
+
+  /// Port, defaults to 5683
+  int port = 5683;
+
+  /// The address
+  InternetAddress address;
+
+  /// Construction
+  /// Default is to use UDP
+  Coap() {
+    _network = new NetworkUDP();
+  }
+
 }
