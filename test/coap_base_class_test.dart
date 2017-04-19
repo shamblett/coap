a1e2c6e9f475d5e2a1892d5667192e72127caf91
Steve Hamblett <steve.hamblett@linux.com>
2017-04-19 14:28:12 +0000

    Options - class + tests

--- test/coap_base_class_test.dart
+++ test/coap_base_class_test.dart
@@ -6,11 +6,29 @@
  */
 import 'package:coap/coap.dart';
 import 'package:test/test.dart';
-//import 'package:typed_data/typed_data.dart' as typed;
+import 'package:typed_data/typed_data.dart' as typed;
+import 'dart:convert';
 
 void main() {
-  group("Options", () {
-    test('Option', () {});
+  group("Infrastructure", () {
+    final Utf8Encoder encoder = new Utf8Encoder();
+
+    test('Option', () {
+      final typed.Uint8Buffer raw = new typed.Uint8Buffer(3);
+      raw.addAll(encoder.convert("raw"));
+      final Option optRaw = Option.createRaw(optionTypeContentType, raw);
+      expect(optRaw.valueBytes, raw);
+      expect(optRaw.type, optionTypeContentType);
+
+      final int oneByteValue = 255;
+      final int twoByteValue = 256;
+      final Option opt1 = Option.createVal(optionTypeContentType, oneByteValue);
+      final Option opt2 = Option.createVal(optionTypeContentType, twoByteValue);
+      expect(opt1.length, 1);
+      expect(opt2.length, 2);
+      expect(opt1.intValue, oneByteValue);
+      expect(opt2.intValue, twoByteValue);
+    });
 
     test('Media types', () {
       final int type = MediaType.applicationJson;
