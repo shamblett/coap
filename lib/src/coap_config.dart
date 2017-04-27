5918ac7637b5e78921f7c504cfa05f20fb076702
Steve Hamblett <steve.hamblett@linux.com>
2017-04-27 12:22:22 +0000

    Config class + tests

--- lib/src/coap_config.dart
+++ lib/src/coap_config.dart
@@ -26,5 +26,18 @@ class CoapConfig extends config.ConfigurationItem {
   /// The initial time (ms) for a CoAP message
   int ackTimeout = CoapConstants.ackTimeout;
 
+  /// The initial timeout is set
+  /// to a random number between RESPONSE_TIMEOUT and (RESPONSE_TIMEOUT *
+  /// RESPONSE_RANDOM_FACTOR)
+  ///
+  double AckRandomFactor = CoapConstants.ackRandomFactor;
+
+  double AckTimeoutScale = 2.0;
+
+  /// The max time that a message would be retransmitted
+  int MaxRetransmit = CoapConstants.maxRetransmit;
+
+  int MaxMessageSize = 1024;
+
 
 }
