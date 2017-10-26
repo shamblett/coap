147b387c2dcc7e04d5b26f6b1245f7fca78fd4ee
Steve Hamblett <steve.hamblett@linux.com>
2017-10-26 10:56:42 +0100

    Message class + tests

--- lib/util/coap_util.dart
+++ lib/util/coap_util.dart
@@ -22,6 +22,62 @@ class Util {
     return ie.equals(first, second);
   }
 
+  /// Finds the first matched item.
+  /// Returns the item found, or null if none is matched.
+  static T firstOrDefault<T>(Iterable<T> source, bool condition(T element)) {
+    return source.firstWhere(condition, orElse: () => null);
+  }
+
+  /// Checks if matched item exists.
+  /// Returns true if exists any matched item, otherwise false.
+  static bool contains<T>(Iterable<T> source, bool condition(T element)) {
+    final res = source.takeWhile(condition);
+    if (res.isNotEmpty) {
+      return true;
+    }
+    return false;
+  }
+
+  /// Stringify a message.
+  static String messageToString(Message msg) {
+    final StringBuffer sb = new StringBuffer();
+    String kind = "Message";
+    String code = "Code";
+    if (msg.isRequest) {
+      kind = "Request";
+      code = "Method";
+    } else if (msg.isResponse) {
+      kind = "Response";
+      code = "Status";
+    }
+    sb.writeln("==[ COAP $kind ]============================================");
+    sb.writeln("ID     : ${msg.id}");
+    sb.writeln("Type   : ${msg.type}");
+    sb.writeln("Token  : ${msg.tokenString}");
+    sb.writeln("Code   : ${msg.codeString}");
+    if (msg.source != null) {
+      sb.writeln("Source : ${msg.source}");
+    }
+    if (msg.destination != null) {
+      sb.writeln("Dest : ${msg.destination}");
+    }
+    sb.writeln("Options: ${ optionsToString(msg)}");
+    sb.writeln("Payload: ${msg.payloadSize} Bytes");
+    if (msg.payloadSize > 0 && MediaType.isPrintable(msg.contentType)) {
+      sb.writeln(
+          "---------------------------------------------------------------");
+      sb.writeln(msg.payloadString);
+    }
+    sb.writeln(
+        "===============================================================");
+    return sb.toString();
+  }
+
+  /// Stringify options in a message.
+  static String optionsToString(Message msg) {
+
+  }
+
   /// Stringify options in a message.
   static String optionsToString(Message msg) {
     return "";
