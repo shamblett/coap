a2a2eeb789bd9dbccd594f5b192b42b58e149500
Steve Hamblett <steve.hamblett@linux.com>
2017-11-01 10:54:21 +0000

    Message class + tests

--- lib/src/coap_response.dart
+++ lib/src/coap_response.dart
@@ -11,4 +11,34 @@ part of coap;
 /// A response is either a piggy-backed response with type ACK
 /// or a separate response with type CON or NON.
 class CoapResponse extends CoapMessage {
+  /// Initializes a response message.
+  CoapResponse(int statusCode)
+      : super.withCode(CoapMessageType.unknown, statusCode) {
+    _statusCode = code;
+  }
+
+  /// The response status code.
+  int _statusCode;
+
+  int get statusCode => _statusCode;
+
+  /// The Round-Trip Time of this response.
+  double rtt;
+
+  /// A value indicating whether this response is the last response of an exchange.
+  bool last = true;
+
+  /// Response text
+  String get responseText => payloadString;
+
+  /// Creates a response to the specified request with the specified response code.
+  /// The destination endpoint of the response is the source endpoint of the request.
+  /// The response has the same token as the request.
+  /// Type and ID are usually set automatically by the ReliabilityLayer>.
+  static CoapResponse createResponse(CoapRequest request, int statusCode) {
+    final CoapResponse response = new CoapResponse(statusCode);
+    response.destination = request.source;
+    response.token = request.token;
+    return response;
+  }
 }
