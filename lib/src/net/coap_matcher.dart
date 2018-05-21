cad1fa86b7bdd162c8735e095e38d622ac351081
Steve Hamblett <steve.hamblett@linux.com>
2018-05-21 15:41:32 +0100

    Matcher and Deduplication classes

--- lib/src/net/coap_matcher.dart
+++ lib/src/net/coap_matcher.dart
@@ -7,10 +7,14 @@
 
 part of coap;
 
-class CoapMatcher implements CoapIMatcher {
+class CoapMatcher extends Object
+    with events.EventEmitter
+    implements CoapIMatcher {
   CoapMatcher(CoapConfig config) {
     _deduplicator = CoapDeduplicatorFactory.createDeduplicator(config);
-    if (config.useRandomIDStart) _currentID = new Random().Next(1 << 16);
+    if (config.useRandomIDStart) {
+      _currentId = new Random().nextInt(1 << 16);
+    }
   }
 
   static CoapILogger _log = new CoapLogManager("console").logger;
@@ -28,6 +32,42 @@ class CoapMatcher implements CoapIMatcher {
       new Map<CoapKeyUri, CoapExchange>();
 
   int _running;
-  int _currentID;
+  int _currentId;
   CoapIDeduplicator _deduplicator;
+
+  void start() {
+    _deduplicator.start();
+  }
+
+  void stop() {
+    _deduplicator.stop();
+    clear();
+  }
+
+  void clear() {
+    _exchangesByID.clear();
+    _exchangesByToken.clear();
+    _ongoingExchanges.clear();
+    _deduplicator.clear();
+  }
+
+  void sendRequest(CoapExchange exchange, CoapRequest request) {
+    if (request.id == CoapMessage.none) request.id = _currentId % (1 << 16);
+
+    // The request is a CON or NON and must be prepared for these responses
+    // - CON => ACK / RST / ACK+response / CON+response / NON+response
+    // - NON => RST / CON+response / NON+response
+    // If this request goes lost, we do not get anything back.
+
+    // The MID is from the local namespace -- use blank address
+    final CoapKeyId keyId = new CoapKeyId(request.id, null);
+    final CoapKeyToken keyToken = new CoapKeyToken(request.token);
+    addEventAction(CoapCompletedEvent, onExchangeCompleted);
+    _log.debug("Stored open request by $keyId + $keyToken");
+
+    _exchangesByID[keyId] = exchange;
+    _exchangesByToken[keyToken] = exchange;
+  }
+
+  void onExchangeCompleted(events.Event<CoapCompletedEvent> event) {}
 }
