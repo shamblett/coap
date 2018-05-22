68577a798dc2cb09bdf5966c27fc8e1225510937
Steve Hamblett <steve.hamblett@linux.com>
2018-05-22 08:30:23 +0100

    Matcher and Deduplication classes

--- lib/src/net/coap_matcher.dart
+++ lib/src/net/coap_matcher.dart
@@ -20,7 +20,7 @@ class CoapMatcher extends Object
   static CoapILogger _log = new CoapLogManager("console").logger;
 
   /// For all
-  Map<CoapKeyId, CoapExchange> _exchangesByID =
+  Map<CoapKeyId, CoapExchange> _exchangesById =
       new Map<CoapKeyId, CoapExchange>();
 
   /// For outgoing
@@ -31,7 +31,6 @@ class CoapMatcher extends Object
   Map<CoapKeyUri, CoapExchange> _ongoingExchanges =
       new Map<CoapKeyUri, CoapExchange>();
 
-  int _running;
   int _currentId;
   CoapIDeduplicator _deduplicator;
 
@@ -45,7 +44,7 @@ class CoapMatcher extends Object
   }
 
   void clear() {
-    _exchangesByID.clear();
+    _exchangesById.clear();
     _exchangesByToken.clear();
     _ongoingExchanges.clear();
     _deduplicator.clear();
@@ -65,7 +64,7 @@ class CoapMatcher extends Object
     addEventAction(CoapCompletedEvent, onExchangeCompleted);
     _log.debug("Stored open request by $keyId + $keyToken");
 
-    _exchangesByID[keyId] = exchange;
+    _exchangesById[keyId] = exchange;
     _exchangesByToken[keyToken] = exchange;
   }
 
@@ -116,12 +115,174 @@ class CoapMatcher extends Object
     if (response.type == CoapMessageType.con ||
         response.type == CoapMessageType.non) {
       final CoapKeyId keyId = new CoapKeyId(response.id, null);
-      _exchangesByID[keyId] = exchange;
+      _exchangesById[keyId] = exchange;
     }
 
     // Only CONs and Observe keep the exchange active
     if (response.type != CoapMessageType.con && response.last) {
       exchange.complete = true;
+    }
+  }
+
+  void sendEmptyMessage(CoapExchange exchange, CoapEmptyMessage message) {
+    if (message.type == CoapMessageType.rst && exchange != null) {
+      // We have rejected the request or response
+      exchange.complete = true;
+    }
+  }
+
+  CoapExchange receiveRequest(CoapRequest request) {
+    // This request could be
+    //  - Complete origin request => deliver with new exchange
+    //  - One origin block        => deliver with ongoing exchange
+    //  - Complete duplicate request or one duplicate block (because client got no ACK)
+    //     =>
+    //		if ACK got lost => resend ACK
+    //		if ACK+response got lost => resend ACK+response
+    //		if nothing has been sent yet => do nothing
+    // (Retransmission is supposed to be done by the retransm. layer)
+
+    CoapKeyId keyId = new CoapKeyId(request.id, request.source);
+
+    // The differentiation between the case where there is a Block1 or
+    // Block2 option and the case where there is none has the advantage that
+    // all exchanges that do not need blockwise transfer have simpler and
+    // faster code than exchanges with blockwise transfer.
+
+    if (!request.hasOption(optionTypeBlock1) &&
+        !request.hasOption(optionTypeBlock2)) {
+      final CoapExchange exchange =
+      new CoapExchange(request, CoapOrigin.remote);
+      final CoapExchange previous = _deduplicator.findPrevious(keyId, exchange);
+      if (previous == null) {
+        addEventAction(CoapCompletedEvent, onExchangeCompleted);
+        return exchange;
+      } else {
+        _log.info("Duplicate request: $request");
+        request.duplicate = true;
+        return previous;
+      }
+    } else {
+      final CoapKeyUri keyUri = new CoapKeyUri(request.uri, request.source);
+      _log.debug("Looking up ongoing exchange for $keyUri");
+
+      final CoapExchange ongoing = _ongoingExchanges[keyUri];
+      if (ongoing != null) {
+        final CoapExchange prev = _deduplicator.findPrevious(keyId, ongoing);
+        if (prev != null) {
+          _log.info("Duplicate ongoing request: $request");
+          request.duplicate = true;
+        } else {
+          // The exchange is continuing, we can (i.e., must) clean up the previous response
+          if (ongoing.currentResponse.type != CoapMessageType.ack &&
+              !ongoing.currentResponse.hasOption(optionTypeObserve)) {
+            keyId = new CoapKeyId(ongoing.currentResponse.id, null);
+            _log.debug("Ongoing exchange got new request, cleaning up $keyId");
+            _exchangesById.remove(keyId);
+          }
+        }
+        return ongoing;
+      } else {
+        // We have no ongoing exchange for that request block.
+
+        // Note the difficulty of the following code: The first message
+        // of a blockwise transfer might arrive twice due to a
+        // retransmission. The new Exchange must be inserted in both the
+        // hash map 'ongoing' and the deduplicator. They must agree on
+        // which exchange they store!
+
+        final CoapExchange exchange =
+        new CoapExchange(request, CoapOrigin.remote);
+        final CoapExchange previous =
+        _deduplicator.findPrevious(keyId, exchange);
+        if (previous == null) {
+          _log.debug("New ongoing request, storing $keyUri for $request");
+          addEventAction(CoapCompletedEvent, onExchangeCompleted);
+          _ongoingExchanges[keyUri] = exchange;
+          return exchange;
+        } else {
+          _log.info("Duplicate initial request: $request");
+          request.duplicate = true;
+          return previous;
+        }
+      } // if ongoing
+    } // if blockwise
+  }
+
+  CoapExchange receiveResponse(CoapResponse response) {
+    // This response could be
+    // The first CON/NON/ACK+response => deliver
+    // Retransmitted CON (because client got no ACK)
+    //	=> resend ACK
+
+    CoapKeyId keyId;
+    if (response.type == CoapMessageType.ack) {
+      // Own namespace
+      keyId = new CoapKeyId(response.id, null);
+    } else {
+      // Remote namespace
+      keyId = new CoapKeyId(response.id, response.source);
+    }
+
+    final CoapKeyToken keyToken = new CoapKeyToken(response.token);
+
+    final CoapExchange exchange = _exchangesByToken[keyToken];
+    if (exchange != null) {
+      // There is an exchange with the given token
+      final CoapExchange prev = _deduplicator.findPrevious(keyId, exchange);
+      if (prev != null) {
+        // (and thus it holds: prev == exchange)
+        _log.info("Duplicate response for open exchange: $response");
+        response.duplicate = true;
+      } else {
+        keyId = new CoapKeyId(exchange.currentRequest.id, null);
+        _log.debug("Exchange got response: Cleaning up $keyId");
+        _exchangesById.remove(keyId);
+      }
+
+      if (response.type == CoapMessageType.ack &&
+          exchange.currentRequest.id != response.id) {
+        // The token matches but not the MID. This is a response for an older exchange
+        _log.warn(
+            "Possible MID reuse before lifetime end: ${response
+                .tokenString} expected MID ${exchange.currentRequest
+                .id} but received ${response.id}");
+      }
+
+      return exchange;
+    } else {
+      // There is no exchange with the given token.
+      if (response.type != CoapMessageType.ack) {
+        // Only act upon separate responses
+        final CoapExchange prev = _deduplicator.find(keyId);
+        if (prev != null) {
+          _log.info("Duplicate response for completed exchange: $response");
+          response.duplicate = true;
+          return prev;
+        }
+      } else {
+        _log.info(
+            "Ignoring unmatchable piggy-backed response from ${response
+                .source} : $response");
+      }
+      // Ignore response
+      return null;
+    }
+  }
+
+  CoapExchange receiveEmptyMessage(CoapEmptyMessage message) {
+    // Local namespace
+    final CoapKeyId keyId = new CoapKeyId(message.id, null);
+    final CoapExchange exchange = _exchangesById[keyId];
+    if (exchange != null) {
+      _log.debug("Exchange got reply: Cleaning up $keyId");
+      _exchangesById.remove(keyId);
+      return exchange;
+    } else {
+      _log.info(
+          "Ignoring unmatchable empty message from ${message
+              .source} : $message");
+      return null;
     }
   }
 
@@ -133,7 +294,7 @@ class CoapMatcher extends Object
     for (CoapResponse previous in relation.clearNotifications()) {
       // Notifications are local MID namespace
       final CoapKeyId keyId = new CoapKeyId(previous.id, null);
-      _exchangesByID.remove(keyId);
+      _exchangesById.remove(keyId);
     }
   }
 }
