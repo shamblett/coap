cf4e5ca9447894ac2337959089b19696380e7ba5
Steve Hamblett <steve.hamblett@linux.com>
2019-02-28 15:19:00 +0000

    Initial testing

--- test/simple_datagram_test.dart
+++ test/simple_datagram_test.dart
@@ -27,22 +27,27 @@ void main() async {
   print('The selected loopback address is $loopbackAddress');
   socket = await RawDatagramSocket.bind(loopbackAddress, 5683);
 
-  /// Send some data
-  print('Sending some data');
   final List<int> sendData = <int>[41, 42, 43, 44];
-  socket.send(sendData, loopbackAddress, 5683);
 
   /// Receive it
   print('Receiving the data');
-  final Datagram rx = socket.receive();
-  if (rx == null) {
-    print('Boo no date received at all!');
-  } else {
-    print('The data is : ${rx.data}');
-    if (const IterableEquality().equals(rx.data, sendData)) {
-      print('Hoorah a match');
+  for (int i = 0; i < 10; i++) {
+    /// Send some data
+    print('Sending some data');
+    int sent = socket.send(sendData, loopbackAddress, 5683);
+    if (sent != sendData.length) {
+      print('Boo, we didnt send 4 ints');
+    }
+    final Datagram rx = socket.receive();
+    if (rx == null) {
+      print('Boo no date received at all!');
     } else {
-      print('Boo no match');
+      print('The data is : ${rx.data}');
+      if (const IterableEquality().equals(rx.data, sendData)) {
+        print('Hoorah a match');
+      } else {
+        print('Boo no match');
+      }
     }
   }
 }
