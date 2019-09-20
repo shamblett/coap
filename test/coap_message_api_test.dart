f5908bde6c9a114ed603c78355960da9049c007d
Steve Hamblett <steve.hamblett@linux.com>
2019-09-20 17:02:12 +0100

    Message API tests

---
+++ test/coap_message_api_test.dart
@@ -0,0 +1,15 @@
+/*
+ * Package : Coap
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 20/09/2019
+ * Copyright :  S.Hamblett
+ */
+import 'dart:io';
+import 'package:coap/coap.dart';
+import 'package:test/test.dart';
+import 'package:typed_data/typed_data.dart' as typed;
+import 'package:collection/collection.dart';
+
+void main() {
+  test('Construction', () {});
+}
