484c9a2ede7d19356fbbd9afc60a24b85ed362bd
Steve Hamblett <steve.hamblett@linux.com>
2019-09-25 12:52:13 +0100

    Message API tests

--- lib/src/coap_defined_address.dart
+++ lib/src/coap_defined_address.dart
@@ -7,11 +7,22 @@
 
 part of coap;
 
+// ignore_for_file: public_member_api_docs
+
 /// Defines standard addresses from RFC7252
 class CoapDefinedAddress {
   /// Multicast IPV4
-  static const String allCOAPIPV4 = '224.0.1.187';
+  static const String allRoutersIPV4 = '224.0.0.2';
+  static const String allSystemsIPV4 = '224.0.0.1';
+  static const String allCOAPNodesIPV4 = '224.0.1.187';
 
   /// Multicast IPV6
-  static const String allCOAPIPV6 = '[FF02::1]';
+  static const String allRoutersIPV6 = '[FF01::2]';
+  static const String allNodesIPV6 = '[FF01::1]';
+  static const String allRoutersLinkLocalIPV6 = '[FF02::2]';
+  static const String allNodesLinkLocalIPV6 = '[FF02::1]';
+  static const String allRoutersSiteLocalIPV6 = '[FF05::2]';
+  static const String allCOAPNodesIPV6 = '[FF01::FD]';
+  static const String allCOAPNodesLinkLocalIPV6 = '[FF02::FD]';
+  static const String allCOAPNodesSiteLocalIPV6 = '[FF05::FD]';
 }
