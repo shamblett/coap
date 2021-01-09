2a5f0452f9298dd54cd316ebfab66f9a79149505
Steve Hamblett <steve.hamblett@linux.com>
2021-01-09 11:24:14 +0000

    Issue 17

--- test/issues/issue11.dart
+++ test/issues/issue11.dart
@@ -1,78 +1,75 @@
 import 'dart:async';
-import 'package:test/test.dart';
 import 'package:coap/coap.dart';
 import 'config/coap_config.dart';
 
 FutureOr main() async {
-  test('Issue 11', () async {
-    const host = 'coap.me';
-    final conf = CoapConfig();
+  const host = 'coap.me';
+  final conf = CoapConfig();
 
-    final uri = Uri(
-        scheme: CoapConstants.uriScheme, host: host, port: conf.defaultPort);
+  final uri =
+      Uri(scheme: CoapConstants.uriScheme, host: host, port: conf.defaultPort);
 
-    final client = CoapClient(uri, conf);
+  final client = CoapClient(uri, conf);
 
-    final path = 'inline';
-    final query = 'status/2/2/1';
-    final pathPut = 'inline';
-    final queryPut = 'lamp';
-    var count = 1;
+  final path = 'inline';
+  final query = 'status/2/2/1';
+  final pathPut = 'inline';
+  final queryPut = 'lamp';
+  var count = 1;
 
-    // Adjust the response timeout if needed, defaults to 32767 milliseconds
-    client.timeout = 10000;
+  // Adjust the response timeout if needed, defaults to 32767 milliseconds
+  client.timeout = 10000;
 
-    void getPut(int count) async {
-      // Create the request for the get request
-      final request = CoapRequest.newGet();
+  void getPut(int count) async {
+    // Create the request for the get request
+    final request = CoapRequest.newGet();
 
-      request.addUriPath(path);
-      request.addUriQuery(query);
+    request.addUriPath(path);
+    request.addUriQuery(query);
 
-      client.request = request;
+    client.request = request;
 
-      print('ISSUE: Sending GET request  - $count...');
-      final response = await client.get();
-      if (response != null) {
-        print('ISSUE: - GET response received  - $count');
-        print(response.payloadString);
-      } else {
-        print('ISSUE: - no response received  - $count');
-        client.cancelRequest();
-        client.close();
-        return;
-      }
-
-      // Create the request for the Put request
-      final requestPut = CoapRequest.newPut();
-
-      requestPut.addUriPath(pathPut);
-      requestPut.addUriQuery(queryPut);
-
-      client.request = requestPut;
-
-      print('ISSUE: Sending PUT request  - $count...');
-      final responsePut = await client.put('The PUT payload');
-
-      if (responsePut != null) {
-        print('ISSUE: - PUT response received  - $count');
-        print(responsePut.payloadString);
-        client.cancelRequest();
-      } else {
-        print('ISSUE: - no response received  - $count');
-        client.cancelRequest();
-        client.close();
-        return;
-      }
+    print('ISSUE: Sending GET request  - $count...');
+    final response = await client.get();
+    if (response != null) {
+      print('ISSUE: - GET response received  - $count');
+      print(response.payloadString);
+    } else {
+      print('ISSUE: - no response received  - $count');
+      client.cancelRequest();
+      client.close();
+      return;
     }
 
-    print('ISSUE: First getPut');
-    await getPut(count);
-    print('ISSUE: Second getPut');
-    await getPut(++count);
+    // Create the request for the Put request
+    final requestPut = CoapRequest.newPut();
 
-    print('ISSUE: closing client');
-    client.close();
-    print('ISSUE: exiting test....');
-  }, timeout: Timeout.factor(4));
+    requestPut.addUriPath(pathPut);
+    requestPut.addUriQuery(queryPut);
+
+    client.request = requestPut;
+
+    print('ISSUE: Sending PUT request  - $count...');
+    final responsePut = await client.put('The PUT payload');
+
+    if (responsePut != null) {
+      print('ISSUE: - PUT response received  - $count');
+      print(responsePut.payloadString);
+      client.cancelRequest();
+    } else {
+      print('ISSUE: - no response received  - $count');
+      client.cancelRequest();
+      client.close();
+      return;
+    }
+  }
+
+  print('ISSUE: First getPut');
+  await getPut(count);
+  print('ISSUE: Second getPut');
+  await getPut(++count);
+
+  print('ISSUE: closing client');
+  client.close();
+  print('ISSUE: exiting test....');
 }
--- test/issues/issue12.dart
+++ test/issues/issue12.dart
@@ -1,41 +1,38 @@
 import 'dart:async';
-import 'package:test/test.dart';
 import 'package:coap/coap.dart';
 import 'config/coap_config.dart';
 
 FutureOr main() async {
-  test('Issue 11', () async {
-    // Create a configuration class. Logging levels can be specified in the
-    // configuration file.
-    final conf = CoapConfig();
+  // Create a configuration class. Logging levels can be specified in the
+  // configuration file.
+  final conf = CoapConfig();
 
-    // Build the request uri, note that the request paths/query parameters can be changed
-    // on the request anytime after this initial setup.
-    const host = 'wsncoap.org';
+  // Build the request uri, note that the request paths/query parameters can be changed
+  // on the request anytime after this initial setup.
+  const host = 'wsncoap.org';
 
-    final uri = Uri(scheme: 'coap', host: host, port: conf.defaultPort);
+  final uri = Uri(scheme: 'coap', host: host, port: conf.defaultPort);
 
-    // Create the client.
-    // The method we are using creates its own request so we do not
-    // need to supply one.
-    // The current request is always available from the client.
-    final client = CoapClient(uri, conf);
+  // Create the client.
+  // The method we are using creates its own request so we do not
+  // need to supply one.
+  // The current request is always available from the client.
+  final client = CoapClient(uri, conf);
 
-    // Create the request for the get request
-    final request = CoapRequest.newGet();
-    request.addUriPath('obs');
-    // Mark the request as observable
-    request.markObserve();
+  // Create the request for the get request
+  final request = CoapRequest.newGet();
+  request.addUriPath('obs');
+  // Mark the request as observable
+  request.markObserve();
 
-    // Getting responses from the observable resource
-    request.responses.listen((CoapResponse response) {
-      print('ISSUE: - payload: ${response.payloadString}');
-    });
+  // Getting responses from the observable resource
+  request.responses.listen((CoapResponse response) {
+    print('ISSUE: - payload: ${response.payloadString}');
+  });
 
-    client.request = request;
+  client.request = request;
 
-    print('ISSUE: - Sending get observable request to '
-        '$host, waiting for responses ....');
-    await client.get();
-  }, timeout: Timeout.factor(8));
+  print('ISSUE: - Sending get observable request to '
+      '$host, waiting for responses ....');
+  await client.get();
 }
