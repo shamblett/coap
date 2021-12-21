4d2c8607fd6a0eb79b7ad3fff863cac0b9c83531
Jan Romann <jan.romann@uni-bremen.de>
2021-12-22 00:22:46 +0100

    fix: use correct byte order for numeric options

--- lib/src/codec/decoders/coap_message_decoder18.dart
+++ lib/src/codec/decoders/coap_message_decoder18.dart
@@ -61,6 +61,15 @@ class CoapMessageDecoder18 extends CoapMessageDecoder {
         // Read option
         final opt = CoapOption.create(currentOption);
         opt.valueBytes = _reader!.readBytes(optionLength);
+        // Reverse byte order for numeric options
+        if (CoapOption.getFormatByType(opt.type) == optionFormat.integer) {
+          final valueBytes = opt.valueBytes;
+          if (valueBytes != null) {
+            final reversedBytes = valueBytes.reversed;
+            opt.valueBytes = typed.Uint8Buffer()..addAll(reversedBytes);
+          }
+        }
+
         message.addOption(opt);
       }
     }
