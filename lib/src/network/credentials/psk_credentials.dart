4b9a99086160d819ec032afc0f33f6b1b52075c5
Jan Romann <jan.romann@uni-bremen.de>
2022-07-11 01:42:53 +0200

    fix: use Uint8List for PskCredentials instead of String

--- lib/src/network/credentials/psk_credentials.dart
+++ lib/src/network/credentials/psk_credentials.dart
@@ -5,13 +5,16 @@
  * Copyright :  Jan Romann
  */
 
+import 'dart:typed_data';
+
 /// Function signature for a callback function for retrieving/generating
 /// [PskCredentials].
 ///
 /// As the format of the [identityHint] is not well-defined, this parameter
 /// can probably be ignored in most cases, when both the identity and the key
 /// are known in advance.
-typedef PskCredentialsCallback = PskCredentials Function(String identityHint);
+typedef PskCredentialsCallback = PskCredentials Function(
+    Uint8List identityHint);
 
 /// Credentials used for PSK Cipher Suites consisting of an [identity]
 /// and a [preSharedKey].
@@ -19,9 +22,9 @@ typedef PskCredentialsCallback = PskCredentials Function(String identityHint);
 /// Currently, only the mandatory Cipher Suite `TLS_PSK_WITH_AES_128_CCM_8` is
 /// supported via tinydtls.
 class PskCredentials {
-  String identity;
+  Uint8List identity;
 
-  String preSharedKey;
+  Uint8List preSharedKey;
 
   PskCredentials({required this.identity, required this.preSharedKey});
 }
