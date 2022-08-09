16d24751a3978e0d556790b39f85b94d1950f72a
Jan Romann <jan.romann@uni-bremen.de>
2022-08-09 13:09:39 +0200

    feat: add min/maxLength and default to OptionTypes

--- lib/src/coap_option_type.dart
+++ lib/src/coap_option_type.dart
@@ -39,55 +39,80 @@ class UnknownCriticalOptionException extends UnknownOptionException {
 /// RFC 7252, Section 12.2 and other CoAP extensions.
 enum OptionType implements Comparable<OptionType> {
   /// C, opaque, 0-8 B, -
-  ifMatch(1, 'If-Match', OptionFormat.opaque),
+  ifMatch(1, 'If-Match', OptionFormat.opaque, minLength: 0, maxLength: 8),
 
   /// C, String, 1-270 B, ""
-  uriHost(3, 'Uri-Host', OptionFormat.string),
+  uriHost(3, 'Uri-Host', OptionFormat.string, minLength: 1, maxLength: 255),
 
   /// E, sequence of bytes, 1-4 B, -
-  eTag(4, 'ETag', OptionFormat.opaque),
+  eTag(4, 'ETag', OptionFormat.opaque, minLength: 1, maxLength: 8),
 
-  ifNoneMatch(5, 'If-None-Match', OptionFormat.empty),
+  ifNoneMatch(
+    5,
+    'If-None-Match',
+    OptionFormat.empty,
+    minLength: 0,
+    maxLength: 0,
+  ),
 
   /// E, Duration, 1 B, 0
-  observe(6, 'Observe', OptionFormat.integer),
+  observe(6, 'Observe', OptionFormat.integer, minLength: 0, maxLength: 3),
 
   /// C, uint, 0-2 B
-  uriPort(7, 'Uri-Port', OptionFormat.integer),
+  uriPort(7, 'Uri-Port', OptionFormat.integer, minLength: 0, maxLength: 2),
 
   /// E, String, 1-270 B, -
-  locationPath(8, 'Location-Path', OptionFormat.string),
+  locationPath(
+    8,
+    'Location-Path',
+    OptionFormat.string,
+    minLength: 0,
+    maxLength: 255,
+  ),
 
   /// C, String, 0-255 B, -
   ///
   /// Defined in [RFC 8613](https://datatracker.ietf.org/doc/html/rfc8613).
   // TODO(JKRhb): Option format should be revisited.
-  oscore(9, 'OSCORE', OptionFormat.opaque),
+  oscore(9, 'OSCORE', OptionFormat.opaque, minLength: 0, maxLength: 255),
 
   /// C, String, 1-270 B, ""
-  uriPath(11, 'Uri-Path', OptionFormat.string),
+  uriPath(11, 'Uri-Path', OptionFormat.string, minLength: 0, maxLength: 255),
 
   /// C, 8-bit uint, 1 B, 0 (text/plain)
-  contentFormat(12, 'Content-Format', OptionFormat.integer),
+  contentFormat(
+    12,
+    'Content-Format',
+    OptionFormat.integer,
+    minLength: 0,
+    maxLength: 2,
+  ),
 
   /// E, variable length, 1--4 B, 60 Seconds
-  maxAge(14, 'Max-Age', OptionFormat.integer),
+  maxAge(
+    14,
+    'Max-Age',
+    OptionFormat.integer,
+    minLength: 0,
+    maxLength: 4,
+    defaultValue: 60,
+  ),
 
   /// C, String, 1-270 B, ""
-  uriQuery(15, 'Uri-Query', OptionFormat.string),
+  uriQuery(15, 'Uri-Query', OptionFormat.string, minLength: 0, maxLength: 255),
 
   /// E, uint, 1 B, 16
   ///
   /// Defined in [RFC 8768](https://datatracker.ietf.org/doc/html/rfc8768).
-  hopLimit(16, 'Hop-Limit', OptionFormat.integer),
+  hopLimit(16, 'Hop-Limit', OptionFormat.integer, minLength: 1, maxLength: 1),
 
   /// C, Sequence of Bytes, 1-n B, -
-  accept(17, 'Accept', OptionFormat.integer),
+  accept(17, 'Accept', OptionFormat.integer, minLength: 0, maxLength: 2),
 
   /// C, uint, 0-3 B, -
   ///
   /// Defined in [RFC 9177](https://datatracker.ietf.org/doc/html/rfc9177).
-  qBlock1(19, 'Q-Block1', OptionFormat.integer),
+  qBlock1(19, 'Q-Block1', OptionFormat.integer, minLength: 0, maxLength: 3),
 
   /// C, empty, 0 B, -
   ///
@@ -98,40 +123,64 @@ enum OptionType implements Comparable<OptionType> {
   /// become an RFC.
   ///
   /// [draft-ietf-core-oscore-edhoc-04]: https://datatracker.ietf.org/doc/html/draft-ietf-core-oscore-edhoc-04#section-3.1
-  edhoc(21, 'EDHOC', OptionFormat.empty),
+  edhoc(21, 'EDHOC', OptionFormat.empty, minLength: 0, maxLength: 0),
 
   /// E, String, 1-270 B, -
-  locationQuery(20, 'Location-Query', OptionFormat.string),
-  block2(23, 'Block2', OptionFormat.integer),
-  block1(27, 'Block1', OptionFormat.integer),
-  size2(28, 'Size2', OptionFormat.integer),
+  locationQuery(
+    20,
+    'Location-Query',
+    OptionFormat.string,
+    minLength: 0,
+    maxLength: 255,
+  ),
+  block2(23, 'Block2', OptionFormat.integer, minLength: 0, maxLength: 3),
+  block1(27, 'Block1', OptionFormat.integer, minLength: 0, maxLength: 3),
+  size2(28, 'Size2', OptionFormat.integer, minLength: 0, maxLength: 4),
 
   /// C, uint, 0-3 B, -
   ///
   /// Defined in [RFC 9177](https://datatracker.ietf.org/doc/html/rfc9177).
-  qBlock2(31, 'Q-Block2', OptionFormat.integer),
+  qBlock2(31, 'Q-Block2', OptionFormat.integer, minLength: 0, maxLength: 3),
 
   /// C, String, 1-270 B, "coap"
-  proxyUri(35, 'Proxy-Uri', OptionFormat.string),
+  proxyUri(35, 'Proxy-Uri', OptionFormat.string, minLength: 1, maxLength: 1034),
 
-  proxyScheme(39, 'Proxy-Scheme', OptionFormat.string),
-  size1(60, 'Size1', OptionFormat.integer),
+  proxyScheme(
+    39,
+    'Proxy-Scheme',
+    OptionFormat.string,
+    minLength: 1,
+    maxLength: 255,
+  ),
+  size1(60, 'Size1', OptionFormat.integer, minLength: 0, maxLength: 4),
 
   /// E, opaque, 1-40 B, -
   ///
   /// Defined in [RFC 9175](https://datatracker.ietf.org/doc/html/rfc9175).
-  echo(252, 'Echo', OptionFormat.opaque),
+  echo(252, 'Echo', OptionFormat.opaque, minLength: 1, maxLength: 40),
 
   /// E, uint, 0-1 B, 0
   ///
   /// Defined in [RFC 7967](https://datatracker.ietf.org/doc/html/rfc7967),
   /// updated by [RFC 8613](https://datatracker.ietf.org/doc/html/rfc8613).
-  noResponse(258, 'No-Response', OptionFormat.integer),
+  noResponse(
+    258,
+    'No-Response',
+    OptionFormat.integer,
+    minLength: 0,
+    maxLength: 1,
+  ),
 
   /// E, opaque, 0-8 B, -
   ///
   /// Defined in [RFC 9175](https://datatracker.ietf.org/doc/html/rfc9175).
-  requestTag(292, 'Request-Tag', OptionFormat.opaque),
+  requestTag(
+    292,
+    'Request-Tag',
+    OptionFormat.opaque,
+    minLength: 1,
+    maxLength: 40,
+  ),
 
   /// C, uint, 2 B, -
   ///
@@ -142,6 +191,8 @@ enum OptionType implements Comparable<OptionType> {
     2049,
     'OCF-Accept-Content-Format-Version',
     OptionFormat.integer,
+    minLength: 2,
+    maxLength: 2,
   ),
 
   /// C, uint, 2 B, -
@@ -153,7 +204,18 @@ enum OptionType implements Comparable<OptionType> {
     2053,
     'OCF-Content-Format-Version',
     OptionFormat.integer,
+    minLength: 2,
+    maxLength: 2,
   );
+
+  const OptionType(
+    this.optionNumber,
+    this.optionName,
+    this.optionFormat, {
+    required this.minLength,
+    required this.maxLength,
+    this.defaultValue,
+  });
 
   /// The number of this option.
   final int optionNumber;
@@ -163,7 +225,14 @@ enum OptionType implements Comparable<OptionType> {
 
   /// The [OptionFormat] of this option (integer, string, opaque, or unknown).
   final OptionFormat optionFormat;
-  const OptionType(this.optionNumber, this.optionName, this.optionFormat);
+
+  /// The minimum length of this [OptionType] in bytes.
+  final int minLength;
+
+  /// The maximum length of this [OptionType] in bytes.
+  final int maxLength;
+
+  final Object? defaultValue;
 
   static final _registry = HashMap.fromEntries(
     values.map((final value) => MapEntry(value.optionNumber, value)),
