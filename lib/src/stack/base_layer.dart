4c4ffcb7661cf386ae9742bf8084b06fb3081d09
Jan Romann <jan.romann@uni-bremen.de>
2023-01-18 16:33:00 +0100

    refactor: clean up stack implementation

--- lib/src/stack/base_layer.dart
+++ lib/src/stack/base_layer.dart
@@ -12,49 +12,56 @@ import '../net/exchange.dart';
 
 /// A partial implementation of a layer.
 abstract class BaseLayer {
-  BaseLayer([this.nextLayer]);
+  BaseLayer? _nextLayer;
 
-  BaseLayer? nextLayer;
+  void addLayer(final BaseLayer layer) {
+    final nextLayer = _nextLayer;
+    if (nextLayer == null) {
+      _nextLayer = layer;
+    } else {
+      nextLayer.addLayer(layer);
+    }
+  }
 
   void sendRequest(
     final CoapExchange initialExchange,
     final CoapRequest request,
   ) {
-    nextLayer?.sendRequest(initialExchange, request);
+    _nextLayer?.sendRequest(initialExchange, request);
   }
 
   void sendResponse(
     final CoapExchange initialExchange,
     final CoapResponse response,
   ) {
-    nextLayer?.sendResponse(initialExchange, response);
+    _nextLayer?.sendResponse(initialExchange, response);
   }
 
   void sendEmptyMessage(
     final CoapExchange initialExchange,
     final CoapEmptyMessage message,
   ) {
-    nextLayer?.sendEmptyMessage(initialExchange, message);
+    _nextLayer?.sendEmptyMessage(initialExchange, message);
   }
 
   void receiveRequest(
     final CoapExchange initialExchange,
     final CoapRequest request,
   ) {
-    nextLayer?.receiveRequest(initialExchange, request);
+    _nextLayer?.receiveRequest(initialExchange, request);
   }
 
   void receiveResponse(
     final CoapExchange initialExchange,
     final CoapResponse response,
   ) {
-    nextLayer?.receiveResponse(initialExchange, response);
+    _nextLayer?.receiveResponse(initialExchange, response);
   }
 
   void receiveEmptyMessage(
     final CoapExchange initialExchange,
     final CoapEmptyMessage message,
   ) {
-    nextLayer?.receiveEmptyMessage(initialExchange, message);
+    _nextLayer?.receiveEmptyMessage(initialExchange, message);
   }
 }
